b6bfe1c5ce44f5624f40d51fd1dba00dda2f75c6
Rony Rozas <rozas.rony@gmail.com>
2015-05-28 12:39:35 +0200

    Create text_processing.py

---
+++ Search Result Relevant/Python/text_processing.py
@@ -0,0 +1,53 @@
+
+def text_normalization_and_tokenization( text , joinTokens=False , specialChars=None ):
+    if specialChars is None:
+        specialChars = set()
+    text = unicode( text )
+    # special chars
+    # remove diatrics
+    # to lower
+    text = strip_accents( replace_special_char( unicode( text ) ) ).lower()
+    # keep az09
+    flagToken = False
+    flagAlpha = False
+    start = 0
+    tokens = []
+    tokens_append = tokens.append
+    for i , c in enumerate( text ):
+        if c in specialChars:
+            if flagToken:
+                flagToken = False
+                tokens_append( text[start:i] )
+            tokens_append( c )
+        elif 'a' <= c <= 'z':
+            if not flagToken :
+                flagToken = True
+                flagAlpha = True
+                start = i
+            elif not flagAlpha:
+                #tokens_append( text[start:i])
+                tokens_append( "<NUM>" )
+                start = i
+                flagAlpha = True
+        elif '0' <= c <= '9':
+            if not flagToken:
+                flagToken = True
+                flagAlpha = False
+                flagNum = True
+                start = i
+            elif flagAlpha:
+                tokens_append( text[start:i])
+                start = i
+                flagAlpha = False
+        elif flagToken:
+            flagToken = False
+            if not flagAlpha:
+                tokens_append( "<NUM>" )
+            else :
+                tokens_append( text[start:i] )
+    if flagToken:
+        tokens_append( text[start:i+1] )
+    if joinTokens:
+        return ' '.join( tokens )
+    else:
+        return tokens
